180dc775448318a7eacae665929ba9ba2f3ce401
Tom Barbette <tom.barbette@uclouvain.be>
2024-02-26 13:50:13 +0100

    Add database support and a test for the sample schema

--- mobility/__init__.py
+++ mobility/__init__.py
@@ -5,9 +5,13 @@ from flask import Flask, render_template
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
+    if test_config:
+        app.config.from_mapping(test_config)
+    else:
+        app.config.from_mapping(
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.root_path, 'poudlard.sqlite'),
+        )
 
     from . import city
     app.register_blueprint(city.bp)
