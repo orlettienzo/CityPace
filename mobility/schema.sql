4ab107d030cf791c059ca9bb2e9ea811bd1a3c52
nescapp <johannes.radesey@student.uclouvain.be>
2024-03-04 14:41:42 +0100

    fix mobile

--- mobility/schema.sql
+++ mobility/schema.sql
@@ -1,18 +1,3 @@
--- DROP TABLE IF EXISTS ville;
--- CREATE TABLE IF NOT EXISTS ville (
---    code_postal INTEGER PRIMARY KEY,
---    nom TEXT NOT NULL,
---    population INTEGER NOT NULL
--- );
--- DROP TABLE IF EXISTS rue;
--- CREATE TABLE IF NOT EXISTS rue (
---    rue_id INTEGER PRIMARY KEY,
---    nom TEXT NOT NULL,
---    code_postal INTEGER,
---    FOREIGN KEY (code_postal) REFERENCES ville(code_postal)
--- );
-
-
 DROP TABLE IF EXISTS ville;
 CREATE TABLE IF NOT EXISTS ville (
     code_postal INTEGER NOT NULL UNIQUE,
