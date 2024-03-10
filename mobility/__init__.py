e77651424e6858454969a1bfd6bc17b0d33eba96
nescapp <johannes.radesey@student.uclouvain.be>
2024-03-10 12:16:06 +0100

    gain d'efficacité de fou et protection brute force

--- mobility/__init__.py
+++ mobility/__init__.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_executor import Executor
 import mobility.csv_converter
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
 
 
 
@@ -11,6 +15,12 @@ def create_app(test_config=None):
     # initialisation des variables
     app = Flask(__name__, instance_relative_config=True)
     executor = Executor(app) # pour le traitement multithread
+    limiter = Limiter(
+        get_remote_address,
+        app=app,
+        default_limits=["200 per day", "50 per hour"],
+        storage_uri="memory://",
+    )
     db_populated = False # pour savoir si la base de données a été peuplée
 
 
@@ -36,12 +46,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # initialisation de la base de données
+
     from . import db
     db.init_app(app)
-
-    with app.app_context():
-        db.init_db()
 
     def populate():
         mobility.csv_converter.populate_db()
@@ -87,16 +94,35 @@ def create_app(test_config=None):
     def robots():
         return 'User-agent: *\nDisallow: /'
     
-    @app.route('/populate')
+    @app.route('/resetdb', methods=['POST'])
+    @limiter.limit("1/minute")
     def populate_task():
-        if not os.path.exists('app_initialized'):
-            executor.submit_stored('populate', populate)
+        # if not os.path.exists('app_initialized'):
+        #     executor.submit_stored('populate', populate)
 
-            with open('app_initialized', 'w') as f:
-                pass
+        #     with open('app_initialized', 'w') as f:
+        #         pass
+
+        #     return 'Populating the database...'
+        # return 'Database already populated'
+
+        data = request.get_json()
+        secret = data.get('secret')
+
+        if secret == "Acgfi9^Ziy!$zpY39CRg4Ww7ZjbmHHwdnbkYYbVen6HN*&ZiY9y$QDU8fB$ED*8tBR!BAwUwA^STjcgXPkUY*oUe*S9YY@D$WEfuK4gA%vDC$mE7&j9tH&Js#6yJJ88D":
+
+            # initialisation de la base de données
+            with app.app_context():
+                db.init_db()
+
+            executor.submit_stored('populate', populate)
+            executor.futures.pop('populate')
 
             return 'Populating the database...'
-        return 'Database already populated'
+        return 'nope'
+
+
+
 
     @app.route('/progress')
     def progress():
