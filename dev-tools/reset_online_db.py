be388ebba11d4cdd38097c57e09f6a9ec16ccbdc
nescapp <johannes.radesey@student.uclouvain.be>
2024-03-10 13:19:21 +0100

    small fix in dev tools

--- dev-tools/reset_online_db.py
+++ dev-tools/reset_online_db.py
@@ -6,7 +6,7 @@ import requests
 
 input("ATTENTION: cette opération va réinitialiser la base de données en ligne. Appuyez sur Entrée pour continuer.")
 
-url = 'http://localhost:5000/resetdb'
+url = 'https://g25.linfo1002.ovh/resetdb'
 data = {'secret': 'Acgfi9^Ziy!$zpY39CRg4Ww7ZjbmHHwdnbkYYbVen6HN*&ZiY9y$QDU8fB$ED*8tBR!BAwUwA^STjcgXPkUY*oUe*S9YY@D$WEfuK4gA%vDC$mE7&j9tH&Js#6yJJ88D'}
 
 response = requests.post(url, json=data)
