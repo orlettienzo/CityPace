a1d5084ced1a250aebff67c8616d3a732ffccfb1
nescapp <johannes.radesey@student.uclouvain.be>
2024-03-19 11:59:25 +0100

    moon utils & tests

--- mobility/tests/schema_test.sql
+++ mobility/tests/schema_test.sql
@@ -2,5 +2,10 @@ PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 
 --Todo : add your test database
+INSERT INTO ville (code_postal, nom, population)
+VALUES
+  (4000, 'Liège', 250000),
+  (5000, 'Namur', 100000);
+
 
 COMMIT;
---
+++ mobility/tests/test_main.py
@@ -0,0 +1,18 @@
+import unittest
+import moon_utils
+import datetime
+
+class TestMoonUtils(unittest.TestCase):
+    def test_moon_phase(self):
+        self.assertAlmostEqual(moon_utils.age(datetime.date(2021, 1, 1)), 17.92, places=2)
+        self.assertAlmostEqual(moon_utils.age(datetime.date(2021, 1, 2)), 18.92, places=2)
+        self.assertAlmostEqual(moon_utils.age(datetime.date(2021, 1, 3)), 19.92, places=2)
+
+    def test_phase(self):
+        my_fixture = datetime.date(2019, 1, 21)
+        expected = moon_utils.MoonPhase.FULL_MOON
+        # test phase with assert calls
+        self.assertEqual(moon_utils.phase(moon_utils.age(my_fixture)), expected, msg=f'My error message on pl.f({my_fixture}) different than {expected}')
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
