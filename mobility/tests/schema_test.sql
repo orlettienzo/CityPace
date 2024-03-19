700f2074edba3180d46b572cab9d70c30f580b1e
Enzo Andrade Orletti <enzo.andrade@student.uclouvain.be>
2024-03-19 13:45:08 +0100

    example 'test_street' avec db en mémoire

--- mobility/tests/schema_test.sql
+++ mobility/tests/schema_test.sql
@@ -1,6 +1,16 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 
---Todo : add your test database
+DROP TABLE IF EXISTS rue;
+CREATE TABLE IF NOT EXISTS rue (
+    rue_id INTEGER,
+    nom TEXT NOT NULL,
+    code_postal INTEGER NOT NULL,
+    PRIMARY KEY (rue_id),
+    FOREIGN KEY (code_postal) REFERENCES ville(code_postal)
+);
+
+INSERT INTO rue (rue_id, nom, code_postal) VALUES (1, 'Rua A', 12345);
+INSERT INTO rue (rue_id, nom, code_postal) VALUES (2, 'Rua B', 12345);
 
 COMMIT;
